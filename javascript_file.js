240fa11cf12d4b471a82125be8378eb14a506592
shakib khan <shakibkhanrock@gmail.com>
2018-02-21 10:23:40 +0600

    javaScripyt added

---
+++ javascript_file.js
@@ -0,0 +1,15 @@
+
+		function confirmation()
+		{
+			var ans=confirm("Are sure to sign in");
+		
+
+		if(ans){
+			//window.location="http://www.google.com/";
+		}
+		else {
+			alert("Thanks for staying with us!");
+		}
+	}
+		
+		

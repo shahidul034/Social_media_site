eba4a63ae093744c1c4ecc9f13e49d6a5277c536
shakib khan <shakibkhanrock@gmail.com>
2018-02-21 21:56:54 +0600

    field Validation added

--- javascript_file.js
+++ javascript_file.js
@@ -36,6 +36,53 @@
 	  document.getElementById("date").innerHTML=day+" "+date+" "+month+" "+year;
 	  document.getElementById("time").innerHTML=hour+":"+min+":"+sec;
     }
+	function confirmation(elem1,elem2)
+		{
+			if(elem1.value.length==0){
+				alert("User name is empty");
+				elem1.focus();
+				return false;
+			}
+			if(elem2.value.length==0){
+			alert("password field is empty");
+			elem2.focus();
+			return false;
+			}
+			else{
+			return true;
+			}
+		
+		}
+function confirmation2(elem1,elem2,elem3,elem4)
+		{
+			if(elem1.value.length==0){
+				alert("First name is empty()");
+				elem1.focus();
+				return false;
+			}
+			if(elem2.value.length==0){
+				alert("Surname name is empty()");
+				elem2.focus();
+				return false;
+			}
+			if(elem3.value.length==0){
+				alert("Phone Number or Email Address is empty()");
+				elem3.focus();
+				return false;
+			}
+			if(elem4.value.length==0){
+			alert("password field is empty");
+			elem4.focus();
+			return false;
+			}
+			else{
+			return true;
+			}
+		
+		}		
+		
+		
+		
 		
 		
 		
